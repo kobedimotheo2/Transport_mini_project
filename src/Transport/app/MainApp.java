0620f13ccd80658ab7adbd03dc986c60d79d4a3a
Motheo Kobedi <motheokobedi04@gmail.com>
2026-04-15 04:34:01 +0300

    Update MainApp.java

--- src/Transport/app/MainApp.java
+++ src/Transport/app/MainApp.java
@@ -1,17 +1,26 @@
+//package com.transport.app;
+
 import java.util.Scanner;
-package com.transport.app;
+import transport.model.Passenger;
+import transport.model.Route;
+import transport.model.Booking;
+import transport.service.BookingService;
+import transport.service.FareCalculator;
+
 public class MainApp{
     public static void main(String []args){
         Scanner sc = new Scanner(System.in);
         int choice ;
     
         while (true) {
-                System.out.println("\n==== Transport System ====");
-                System.out.println("1. View Routes");
-                System.out.println("2. Book Seat");
-                System.out.println("3. View Bookings");
-                System.out.println("4. Calculate Fare");
-                System.out.println("5. Exit\n");
+                System.out.println("\n_____________________________");
+                System.out.println("|===== Transport System =====|");
+                System.out.println("|1. View Routes              |");
+                System.out.println("|2. Book Seat                |");
+                System.out.println("|3. View Bookings            |");
+                System.out.println("|4. Calculate Fare           |");
+                System.out.println("|5. Exit                     |");
+                System.out.println("|____________________________|\n");
                 System.out.print("Choose option: ");
 
                 choice = sc.nextInt();
@@ -23,17 +32,34 @@ public class MainApp{
                 if (choice >= 1 && choice <= 4){
                         if(choice == 1){
                             System.out.println("Displaying routes...\n");
+
+                            //System.out.println("Available routes");
+                            //System.out.println(route1.getRouteInfo() +" Distance" + route1.getDistance() + " km");
                             
                         }else if(choice == 2){
                             System.out.println("Booking seat...\n");
+
+                            System.out.println("Enter Passenger name: ");
+                            String name = sc.nextLine();
+                            System.out.println("Enter Passenger phone number: ");
+                            String phone = sc.nextLine();
+
+                            //Passenger passenger = new Passenger(name , phone);
+
+                            //Booking booking = new Booking (passenger, route1);
+
+                            //bookingService.addBooking(booking);
                     
                         }else if(choice == 3){
                             System.out.println("Viewing bookings...\n");
+                            //bookingService.viewBookings();
                                 
                         }else if(choice == 4){
-                            System.out.println("Calculating fare...\n");}
-                        
+                            System.out.println("Calculating fare...\n");
+                            //double fare = fareCalculator.calculateFare(route1.getDistance());
+                            //System.out.println("Fare for "+ route1.getRouteinfo()+ "is: P" + fare);
                             
+                        }     
                         }
                         else {
                             System.out.println("please choose the correct choice");
@@ -43,4 +69,6 @@ public class MainApp{
         sc.close();
     }
 }
+    
+ 
     
